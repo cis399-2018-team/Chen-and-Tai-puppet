12002c889ee2a90352233eb686361acc2c71a20d
Injoker <binbon333@gmail.com>
2018-07-11 16:11:44 -0700

    delete cron

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,17 +1,7 @@
 node ip-10-0-3-230 {
-	cron { "puppet update":
-		command => "cd /etc/puppet && git pull -q origin master",
-		user    => root,
-		minute  => "*/5",
-	}
 }
 
 node ip-10-0-3-20 {
-	cron { "puppet update":
-		command => "cd /etc/puppet && git pull -q origin master",
-		user    => root,
-		minute  => "*/5",
-	}
 }
 
 

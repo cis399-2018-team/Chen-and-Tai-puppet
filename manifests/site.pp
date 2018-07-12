1def7facf61891f577a0f75b7623c6b08b80d9f7
Injoker <binbon333@gmail.com>
2018-07-12 13:35:26 -0700

    set up cron job

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,3 +6,11 @@ node ip-10-0-3-20 {
 
 node ip-10-0-3-167 {
 }
+# update /etc/puppet on puppet master (use puppet master node name)
+node ip-10-0-x-y {
+    cron { "puppet update":
+	    command => "cd /etc/puppet && git pull -q origin master",
+	    user    => root,
+	    minute  => "*/5",
+    }
+}

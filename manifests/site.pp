db96a37db43178d7a5a10fc5a8370bd65556e7ab
Mantai Lin <mantail@uoregon.edu>
2018-07-12 16:30:40 -0700

    update

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,16 @@
 node ip-10-0-3-230 {
+	include sshd
 }
 
 node ip-10-0-3-20 {
+	include sshd
 }
 
 # update /etc/puppet on puppet master (use puppet master node name)
 node ip-10-0-3-167 {
-    cron { "puppet update":
+	include sshd
+
+	cron { "puppet update":
 	    command => "cd /etc/puppet && git pull -q origin master",
 	    user    => root,
 	    minute  => "*/5",
--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -22,15 +22,19 @@ class sshd {
 			enable	  => true,
 			# restart service if it is not running
 			ensure	  => running,
-			# "service sshd status" returns useful service status info
-			hasstatus => true,
-			# package and configuration must be present for service
-			require   => [ Package["openssh-server"],
-						   File["/etc/sshd.conf"] ],
 			# changes to configuration cause service restart
-			subscribe => File["/etc/sshd.conf"],
+			subscribe => File["/etc/ssh/sshd_config"],
+			# notifying sshd when it changes
+			notify    => [ File["/etc/sshd.conf"] ],
 			}
 
+	ssh_authorized_key1 { "mntnttil":
+			user => "ubuntu",
+			key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC9BuBjjJyPbszba4CItoNN8D7OqYY9qhPK+r4givMcs9mam544FvC4pIvRGr++emaPRlGUo/VJgPzm5l/m6XCrkLTfJZbPozHGdE27rgPY1dfZav+1TMahVo4pFs9O3AqQ8At92PkgxzdgorreCC9bLqFm4gEFIk2liWYynKATxKXyqBA4pZ9WT4tjo4XDkuHMQ+DfWcYWwMeIu/TqMJcLAKQZEznQ0RExwfX7NNK1PASp621ocekShbQ09ui3dCWFzT081mZwy/f3ejHFGKmPNdRsHsb4epkW8TFZVSB7/k+gETFbvdcLFW11VJwpHLMMCUYOAxbO0qOundqIxxk/",
+	}
 	
-
+	ssh_authorized__key2 { "injoker1":
+			user => "ubuntu",
+			key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCbdoZIN0kuw7SZd428Ibv0fsrmijw3lXun+AYQgvcfGhwHNO/fRgGS1LeaEfcIhzJQ/86r2OOMpO0C9w9vnkXSPbspFGofzQu2nH5w7a12i91d47FLd6wVbT9FQYJC+IbYVl1Xw3S9KUsMwgA9ovLfy00ejRWPf2sH/DjCNWCKbXWn7s9B4EWGeClIfrs0FCP+YZ3RsZfahn/oB1FT9dNqHrHaKNjd2TlDWu3oBVJJEUNPZml8gnD/tOhFaJbdZdTU+qNtFHv+AlXrhixhvax0UG2Y/c22ofWOyxeqYS8xgoQRpugkBIGaILkAa0IzB7BzhlfU0QYCOnR1K7D9QAqj",
+	}
 }

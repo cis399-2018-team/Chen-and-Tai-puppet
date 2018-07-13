57207c3f14d6dc0febe8aaf08f71d225422ade64
Injoker <binbon333@gmail.com>
2018-07-12 17:18:24 -0700

    edit

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -29,10 +29,10 @@ class sshd {
 			}
 
 	
-	ssh_authorized__key { "injokerCIS399":
+	ssh_authorized_key { "injokerCIS399":
 			ensure =>present,
 			user => "ubuntu",
-			type => "ssh-rsa",
+			type => "ssh-rsa"
 			key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCbdoZIN0kuw7SZd428Ibv0fsrmijw3lXun+AYQgvcfGhwHNO/fRgGS1LeaEfcIhzJQ/86r2OOMpO0C9w9vnkXSPbspFGofzQu2nH5w7a12i91d47FLd6wVbT9FQYJC+IbYVl1Xw3S9KUsMwgA9ovLfy00ejRWPf2sH/DjCNWCKbXWn7s9B4EWGeClIfrs0FCP+YZ3RsZfahn/oB1FT9dNqHrHaKNjd2TlDWu3oBVJJEUNPZml8gnD/tOhFaJbdZdTU+qNtFHv+AlXrhixhvax0UG2Y/c22ofWOyxeqYS8xgoQRpugkBIGaILkAa0IzB7BzhlfU0QYCOnR1K7D9QAqj",
 	}
 }

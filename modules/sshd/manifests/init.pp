9a4b4c5a6725efde0c9a6b8baeb2f269632bd60f
Injoker <binbon333@gmail.com>
2018-07-12 17:25:01 -0700

    edit

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -26,13 +26,11 @@ class sshd {
 			subscribe => File["/etc/ssh/sshd_config"],
 			# notifying sshd when it changes
 			notify    => [ File["/etc/sshd.conf"] ],
-			}
+	}
 
 	
 	ssh_authorized_key { "injokerCIS399":
-			ensure =>present,
 			user => "ubuntu",
-			type => "ssh-rsa"
 			key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCbdoZIN0kuw7SZd428Ibv0fsrmijw3lXun+AYQgvcfGhwHNO/fRgGS1LeaEfcIhzJQ/86r2OOMpO0C9w9vnkXSPbspFGofzQu2nH5w7a12i91d47FLd6wVbT9FQYJC+IbYVl1Xw3S9KUsMwgA9ovLfy00ejRWPf2sH/DjCNWCKbXWn7s9B4EWGeClIfrs0FCP+YZ3RsZfahn/oB1FT9dNqHrHaKNjd2TlDWu3oBVJJEUNPZml8gnD/tOhFaJbdZdTU+qNtFHv+AlXrhixhvax0UG2Y/c22ofWOyxeqYS8xgoQRpugkBIGaILkAa0IzB7BzhlfU0QYCOnR1K7D9QAqj",
 	}
 }

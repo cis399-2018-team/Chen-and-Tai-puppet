93a5f7a1dbea2bd6265dd603f63888be32ac0d45
Injoker <binbon333@gmail.com>
2018-07-22 21:34:28 -0700

    update module

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -4,10 +4,8 @@ class apache {
 	}
 
 	file { "/etc/apache2/apache2.conf":
-			source => [
-					"puppet:///modules/apache/apache2.conf",
-					],
-			mode => 444,
+			source => "puppet:///modules/apache/apache2.conf",
+			mode => 644,
 			owner => root,
 			group => root,
 			require => Package["appache2"],
@@ -19,6 +17,16 @@ class apache {
 			enable => ture,
 			require => [Package["apache2"], File["/etc/apache2/apache2.conf"]],
 			subscribe => File["/etc/apache2/apache2.conf"]
+
+	}
+	file { "/var/www/html":
+			ensure => directory,
+			require => package["apache2"],
+			recurse => true,
+			owner => root,
+			group => root,
+			source => "puppet:///modules/apache/html",
+			mode => 644,
 
 	}
 }

9bcafcf6c9832626ec4e385b017299e9a0decfc5
Injoker <binbon333@gmail.com>
2018-07-22 22:03:06 -0700

    edit

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,7 +11,6 @@ node ip-10-0-3-20 {
 # update /etc/puppet on puppet master (use puppet master node name)
 node ip-10-0-3-167 {
 	include sshd
-	include apache
 	
 	cron { "puppet update":
 	    command => "cd /etc/puppet && git pull -q origin master",

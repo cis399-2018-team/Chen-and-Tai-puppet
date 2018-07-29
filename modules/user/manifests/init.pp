a71b65217911b0dfe9485ae551946d5ec13327d9
Injoker <binbon333@gmail.com>
2018-07-29 01:30:01 -0700

    test

--- modules/user/manifests/init.pp
+++ modules/user/manifests/init.pp
@@ -5,10 +5,32 @@ class user {
 			home => '/home/mhagel2',
 			managehome => 'true',
 			shell => '/bin/bash';
+
+			'jbeder1':
+			ensure => 'present',
+			home => '/home/jbeder1',
+			managehome => 'true',
+			shell => '/bin/bash';
+
+			'dbeeman':
+			ensure => 'present',
+			home => '/home/dbeeman',
+			managehome => 'true',
+			shell => '/bin/bash';
 		}
 
 	ssh_authorized_key { 'mhagel2':
 						  user => 'mhagel2',
+						  type => 'ssh-rsa',
+						  key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCbdoZIN0kuw7SZd428Ibv0fsrmijw3lXun+AYQgvcfGhwHNO/fRgGS1LeaEfcIhzJQ/86r2OOMpO0C9w9vnkXSPbspFGofzQu2nH5w7a12i91d47FLd6wVbT9FQYJC+IbYVl1Xw3S9KUsMwgA9ovLfy00ejRWPf2sH/DjCNWCKbXWn7s9B4EWGeClIfrs0FCP+YZ3RsZfahn/oB1FT9dNqHrHaKNjd2TlDWu3oBVJJEUNPZml8gnD/tOhFaJbdZdTU+qNtFHv+AlXrhixhvax0UG2Y/c22ofWOyxeqYS8xgoQRpugkBIGaILkAa0IzB7BzhlfU0QYCOnR1K7D9QAqj';
+
+						  'jbeder1':
+						  user => 'jbeder1',
+						  type => 'ssh-rsa',
+						  key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCbdoZIN0kuw7SZd428Ibv0fsrmijw3lXun+AYQgvcfGhwHNO/fRgGS1LeaEfcIhzJQ/86r2OOMpO0C9w9vnkXSPbspFGofzQu2nH5w7a12i91d47FLd6wVbT9FQYJC+IbYVl1Xw3S9KUsMwgA9ovLfy00ejRWPf2sH/DjCNWCKbXWn7s9B4EWGeClIfrs0FCP+YZ3RsZfahn/oB1FT9dNqHrHaKNjd2TlDWu3oBVJJEUNPZml8gnD/tOhFaJbdZdTU+qNtFHv+AlXrhixhvax0UG2Y/c22ofWOyxeqYS8xgoQRpugkBIGaILkAa0IzB7BzhlfU0QYCOnR1K7D9QAqj';
+
+						  'dbeeman':
+						  user => 'dbeeman',
 						  type => 'ssh-rsa',
 						  key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCbdoZIN0kuw7SZd428Ibv0fsrmijw3lXun+AYQgvcfGhwHNO/fRgGS1LeaEfcIhzJQ/86r2OOMpO0C9w9vnkXSPbspFGofzQu2nH5w7a12i91d47FLd6wVbT9FQYJC+IbYVl1Xw3S9KUsMwgA9ovLfy00ejRWPf2sH/DjCNWCKbXWn7s9B4EWGeClIfrs0FCP+YZ3RsZfahn/oB1FT9dNqHrHaKNjd2TlDWu3oBVJJEUNPZml8gnD/tOhFaJbdZdTU+qNtFHv+AlXrhixhvax0UG2Y/c22ofWOyxeqYS8xgoQRpugkBIGaILkAa0IzB7BzhlfU0QYCOnR1K7D9QAqj';
 
